aa3aae64c3c400073b294812d93de8fda3c8f494
muhammadammar2 <uExistentialist@proton.me>
2024-10-22 22:34:04 +0500

    a bit of sockets

--- server/socketUtils.c
+++ server/socketUtils.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <winsock2.h>
@@ -8,3 +7,16 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
+
+int createSocket() {
+    int server_socket = socket(AF_INET , SOCK_STREAM , 0);
+    if(server_socket < 0) {
+        perror("Socket connection failed");
+        exit(EXIT_FAILURE);
+    } 
+    return server_socket;
+}
+
+void bindSocket(int server_socket , int port) {
+    
+}
