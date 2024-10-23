df1c30e397573fa7a1d43fe29e0bc1808a741375
muhammadammar2 <uExistentialist@proton.me>
2024-10-23 06:27:57 +0500

    structured

--- headers/socketUtils.h
+++ headers/socketUtils.h
@@ -4,9 +4,13 @@
 #include <winsock2.h>
 
 int createSocket();
+
 void bindSocket(int server_socket , int port);
+
 void listenForConnections(int server_socket);
+
 int acceptConnections(int serever_socket , struct sockaddr_in * client_addr);
+
 void cleanupSocket(int socket);
 
 #endif
