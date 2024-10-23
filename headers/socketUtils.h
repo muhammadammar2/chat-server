25f372edfcadf76ba5974400ead986b26e8a50cc
muhammadammar2 <uExistentialist@proton.me>
2024-10-23 05:38:51 +0500

    some sockets configs

--- headers/socketUtils.h
+++ headers/socketUtils.h
@@ -7,5 +7,6 @@ int createSocket();
 void bindSocket(int server_socket , int port);
 void listenForConnections(int server_socket);
 int acceptConnections(int serever_socket , struct sockaddr_in * client_addr);
+void cleanupSocket(int socket);
 
 #endif
--- server/socketUtils.c
+++ server/socketUtils.c
@@ -5,18 +5,62 @@
 
 #include "socketUtils.h"
 
+// link with Ws2_32.lib
 #pragma comment(lib, "Ws2_32.lib")
 
 
 int createSocket() {
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2 , 2) , &wsaData);
+    if(iResult != 0) {
+        printf("WSAStartup failed: %d\n" , iResult);
+        exit (EXIT_FAILURE);
+    }
+   // for tcp connection
     int server_socket = socket(AF_INET , SOCK_STREAM , 0);
-    if(server_socket < 0) {
-        perror("Socket connection failed");
+    if(server_socket == INVALID_SOCKET) {
+        printf("Socket creation failed: %d\n" , WSAGetLastError());
+        WSACleanup();
         exit(EXIT_FAILURE);
-    } 
+    }
     return server_socket;
 }
 
+
 void bindSocket(int server_socket , int port) {
-    
+    struct sockaddr_in server_addr;
+    //for configuration of server
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
+    if(bind(server_socket , (struct sockaddr*)&server_addr , sizeof(server_addr)) == SOCKET_ERROR) {
+        printf("Bind failed: %d\n", WSAGetLastError());
+        closesocket(server_socket);
+        WSACleanup();
+        exit(EXIT_FAILURE);
+    }
+}
+
+void listenForConnections(int server_socket) {
+    if(listen(server_socket , 10) == SOCKET_ERROR) {
+        printf("Listen failed: %d\n" , WSAGetLastError());
+        closesocket(server_socket);
+        WSACleanup();
+        exit(EXIT_FAILURE);
+    }
+}
+
+int acceptConnections (int server_socket , struct sockaddr_in *client_addr) {
+    int client_addr_ln = sizeof(*client_addr);
+    int client_socket = accept(server_socket , (struct sockaddr*)client_addr , &client_addr_ln);
+    if(client_socket == INVALID_SOCKET) {
+        printf("Accept failed: %d\n" , WSAGetLastError());
+    }
+    return client_socket;
+}
+
+void cleanupSocket(int socket) {
+    closesocket(socket);
+    WSACleanup();
 }
